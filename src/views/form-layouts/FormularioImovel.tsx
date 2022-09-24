ca6169d5583755cbd9ed82236d9654e8e77acf5a
Wilson Carvalho <wilson@fuerzastudio.com.br>
2022-09-24 17:50:34 -0300

    feat: add states to FormularioImovel

--- src/views/form-layouts/FormularioImovel.tsx
+++ src/views/form-layouts/FormularioImovel.tsx
@@ -9,7 +9,8 @@ import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
 import CardHeader from "@mui/material/CardHeader";
 import CardContent from "@mui/material/CardContent";
-
+import Select, { SelectChangeEvent } from "@mui/material/Select";d
+import MenuItem from "@mui/material/MenuItem";
 
 interface State {
   password: string;
@@ -17,86 +18,146 @@ interface State {
 }
 
 const FormularioImovel = () => {
-  // ** States
-  const [values, setValues] = useState<State>({
-    password: "",
-    showPassword: false,
-  });
-  const [confirmPassValues, setConfirmPassValues] = useState<State>({
-    password: "",
-    showPassword: false,
-  });
+  const [disponivel, setDisponivel] = useState(1);
+  const [area, setArea] = useState(0);
+  const [iptu, setIptu] = useState(0);
 
-  const handleChange =
-    (prop: keyof State) => (event: ChangeEvent<HTMLInputElement>) => {
-      setValues({ ...values, [prop]: event.target.value });
+  const [logradouro, setLogradouro] = useState("");
+  const [cidade, setCidade] = useState("");
+  const [estado, setEstado] = useState("");
+  const [cep, setCep] = useState("");
+  const [pais, setPais] = useState("");
+  const [complemento, setComplemento] = useState("");
+  const [numero, setNumero] = useState("");
+
+  const handleSubmit = (e: any) => {
+    const imovel = {
+      disponivel: !!disponivel,
+      area,
+      iptu,
     };
 
-  const handleConfirmPassChange =
-    (prop: keyof State) => (event: ChangeEvent<HTMLInputElement>) => {
-      setConfirmPassValues({
-        ...confirmPassValues,
-        [prop]: event.target.value,
-      });
+    const endereco = {
+      logradouro,
+      cidade,
+      estado,
+      cep,
+      pais,
+      complemento,
+      numero,
     };
-  const handleClickShowPassword = () => {
-    setValues({ ...values, showPassword: !values.showPassword });
-  };
 
-  const handleClickConfirmPassShow = () => {
-    setConfirmPassValues({
-      ...confirmPassValues,
-      showPassword: !confirmPassValues.showPassword,
+    console.log({
+      ...imovel,
+      ...endereco,
     });
-  };
 
-  const handleMouseDownPassword = (event: MouseEvent<HTMLButtonElement>) => {
-    event.preventDefault();
+    e.preventDefault();
   };
-
   return (
     <Card>
-      <CardContent>
-      <CardHeader title="Sobre o imóvel" titleTypographyProps={{ variant: "h6" }} />
-        <form onSubmit={(e) => e.preventDefault()}>
+      <form onSubmit={handleSubmit}>
+        <CardContent>
+          <CardHeader
+            title="Sobre o imóvel"
+            titleTypographyProps={{ variant: "h6" }}
+          />
           <Grid container spacing={5}>
             <Grid item xs={12}>
-              <TextField fullWidth label="Disponível" />
+              <Select
+                autoWidth
+                value={disponivel}
+                onChange={(value) =>
+                  setDisponivel(value.target.value as number)
+                }
+              >
+                <MenuItem value={1}>Disponível</MenuItem>
+                <MenuItem value={0}>Indisponível</MenuItem>
+              </Select>
             </Grid>
             <Grid item xs={12}>
-              <TextField fullWidth label="Área" />
+              <TextField
+                fullWidth
+                type="number"
+                label="Área"
+                value={area}
+                onChange={(value) => setArea(value.target.value as any)}
+              />
             </Grid>
             <Grid item xs={12}>
-              <TextField fullWidth label="IPTU" />
+              <TextField
+                fullWidth
+                type="number"
+                label="IPTU"
+                value={iptu}
+                onChange={(value) => setIptu(value.target.value as any)}
+              />
             </Grid>
           </Grid>
-        </form>
-      </CardContent>
+        </CardContent>
 
-      <CardContent>
-      <CardHeader title="Endereço" titleTypographyProps={{ variant: "h6" }} />
-        <form onSubmit={(e) => e.preventDefault()}>
+        <CardContent>
+          <CardHeader
+            title="Endereço"
+            titleTypographyProps={{ variant: "h6" }}
+          />
+
           <Grid container spacing={5}>
             <Grid item xs={12}>
-              <TextField fullWidth label="Logradouro" />
+              <TextField
+                fullWidth
+                label="Logradouro"
+                value={logradouro}
+                onChange={(value) => setLogradouro(value.target.value)}
+              />
             </Grid>
             <Grid item xs={12}>
-              <TextField fullWidth label="Número" />
+              <TextField
+                fullWidth
+                label="Número"
+                value={numero}
+                onChange={(value) => setNumero(value.target.value)}
+              />
             </Grid>
             <Grid item xs={12}>
-              <TextField fullWidth label="Complemento" />
+              <TextField
+                fullWidth
+                label="Complemento"
+                value={complemento}
+                onChange={(value) => setComplemento(value.target.value)}
+              />
             </Grid>
             <Grid item xs={12}>
-              <TextField fullWidth label="CEP" />
+              <TextField
+                fullWidth
+                label="CEP"
+                value={cep}
+                onChange={(value) => setCep(value.target.value)}
+              />
             </Grid>
             <Grid item xs={12}>
-              <TextField fullWidth label="Cidade" />
+              <TextField
+                fullWidth
+                label="Cidade"
+                value={cidade}
+                onChange={(value) => setCidade(value.target.value)}
+              />
             </Grid>
             <Grid item xs={12}>
-              <TextField fullWidth label="Estado" />
+              <TextField
+                fullWidth
+                label="Estado"
+                value={estado}
+                onChange={(value) => setEstado(value.target.value)}
+              />
             </Grid>
             <Grid item xs={12}>
-              <TextField fullWidth label="País" />
+              <TextField
+                fullWidth
+                label="País"
+                value={pais}
+                onChange={(value) => setPais(value.target.value)}
+              />
             </Grid>
 
             <Grid item xs={12}>
@@ -115,8 +176,8 @@ const FormularioImovel = () => {
               </Box>
             </Grid>
           </Grid>
-        </form>
-      </CardContent>
+        </CardContent>
+      </form>
     </Card>
   );
 };
