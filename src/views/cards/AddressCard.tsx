c5ccaca9dd56c8d7cd76612093c5b68620a078e6
Rafael Sena <rafaelsenna13@gmail.com>
2022-09-23 23:24:34 -0300

    feat: added property details page

--- src/views/cards/AddressCard.tsx
+++ src/views/cards/AddressCard.tsx
@@ -10,6 +10,7 @@ const AddressCard: React.FC<AddressCardProps> = ({ endereco }) => {
   return (
     <Card>
       <CardContent>
+        <Typography marginBottom={8} variant='h5'>Informações do Endereço</Typography>
         <Typography variant='body2'>{endereco.pais}</Typography>
         <Typography variant='body2'>{endereco.estado}, {endereco.cidade}</Typography>
         <Typography variant='body2'>{endereco.logradouro} - {endereco.numero}, {endereco.cep}</Typography>
--- src/views/cards/PropertyCard.tsx
+++ src/views/cards/PropertyCard.tsx
@@ -1,33 +1,64 @@
 // ** MUI Imports
-import Box from '@mui/material/Box'
-import Card from '@mui/material/Card'
-import Button from '@mui/material/Button'
-import Typography from '@mui/material/Typography'
-import CardContent from '@mui/material/CardContent'
-import CardActions from '@mui/material/CardActions'
-import { Imovel } from 'src/models'
-import AddressCard from './AddressCard'
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import { Imovel } from "src/models";
+import AddressCard from "./AddressCard";
+import { CurrencyBrl, Home, CheckCircle, CloseCircle } from "mdi-material-ui";
 
 type PropertyCardProps = {
-  imovel: Imovel
-}
+  imovel: Imovel;
+  onCreateAdClick: Function;
+  onEditPropertyClick: Function;
+};
 
 const PropertyCard: React.FC<PropertyCardProps> = ({ imovel }) => {
   return (
     <Card>
       <CardContent>
-        <Box sx={{ mb: 5, display: 'flex', flexWrap: 'wrap', alignItems: 'center' }}>
-          <Typography variant='body2'>{imovel.tipo} | {imovel.area} m²</Typography>
-          <Typography variant='body2'>{imovel.disponivel}</Typography>
+        <Box
+          sx={{
+            mb: 1,
+            ml: 8,
+            display: "flex",
+            flexWrap: "wrap",
+            alignItems: "center",
+          }}
+        >
+          <Typography variant="body2">
+            {`${imovel.tipo} | ${imovel.area} m²`}
+          </Typography>
         </Box>
-        <Box><AddressCard endereco={imovel.endereco} /></Box>
+        <Box marginBottom={5}>
+          <Typography>
+            {imovel.disponivel ? (
+              <Typography display="flex" alignItems="center" variant="body2">
+                <CheckCircle color="success" />
+                <Box marginLeft={2}>Imóvel disponível para anúncio</Box>
+              </Typography>
+            ) : (
+              <Box>
+                <Typography display="flex" alignItems="center" variant="body2">
+                  <CloseCircle color="warning" />
+                  <Box marginLeft={2}>Imóvel não disponível para anúncio</Box>
+                </Typography>
+              </Box>
+            )}
+          </Typography>
+        </Box>
+        <Box>
+          <AddressCard endereco={imovel.endereco} />
+        </Box>
       </CardContent>
-      <CardActions className='card-action-dense'>
+      <CardActions className="card-action-dense">
         <Button>Editar Imóvel</Button>
-        <Button>Criar Anúncio</Button>
+        {imovel.disponivel && <Button>Criar Anúncio</Button>}
       </CardActions>
     </Card>
-  )
-}
+  );
+};
 
-export default PropertyCard
+export default PropertyCard;
