517679588bb3b3bb5b083e48022af9566df669e7
Brabec <lu_brabec@hotmail.com>
2022-10-25 13:01:08 -0300

    Criação de Funcionario via seed

--- src/seed.ts
+++ src/seed.ts
@@ -1,9 +1,30 @@
 import { PrismaClient } from '@prisma/client'
 import { add } from 'date-fns'
+import { bcrypt } from 'bcryptjs'
 
 const prisma = new PrismaClient()
 
-async function main() {}
+async function main() {
+    // reset db
+    await prisma.anuncio.deleteMany({})
+    await prisma.cliente.deleteMany({})
+    await prisma.imovel.deleteMany({})
+    await prisma.funcionario.deleteMany({})
+    await prisma.tipo.deleteMany({})
+    await prisma.endereco.deleteMany({})
+    await prisma.contrato.deleteMany({})
+    await prisma.interessado.deleteMany({})
+
+    // insert records
+    const funcionario = await prisma.funcionario.create({
+        data: {
+            nome: 'Ze pezão',
+            email: 'ze@email.com',
+            password: bcrypt.hashSync('password', 10)
+        }
+    })
+    
+}
 
 main ()
     .catch((e: Error) => {
