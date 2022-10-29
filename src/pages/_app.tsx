054d5d2580ecc4fd047e6bccc425db195e2e95d0
Rafael Sena <rafaelsenna13@gmail.com>
2022-10-28 21:47:11 -0300

    feat: updated app name

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -67,14 +67,10 @@ const App = (props: ExtendedAppProps) => {
   return (
     <CacheProvider value={emotionCache}>
       <Head>
-        <title>{`${themeConfig.templateName} - Material Design React Admin Template`}</title>
+        <title>Kit In Net </title>
         <meta
           name="description"
-          content={`${themeConfig.templateName} – Material Design React Admin Dashboard Template – is the most developer friendly & highly customizable Admin Dashboard Template based on MUI v5.`}
-        />
-        <meta
-          name="keywords"
-          content="Material Design, MUI, Admin Template, React Admin Template"
+          content="Sua plataforma para gestão de imóveis"
         />
         <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
