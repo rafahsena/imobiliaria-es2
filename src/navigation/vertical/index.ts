faf93c89faf109dc6f483735b293b64d55aef990
Rafael Sena <rafaelsenna13@gmail.com>
2022-10-30 19:01:53 -0300

    feat: adicionado opções do menu

--- src/navigation/vertical/index.ts
+++ src/navigation/vertical/index.ts
@@ -3,34 +3,49 @@ import Login from "mdi-material-ui/Login";
 import HomeOutline from "mdi-material-ui/HomeOutline";
 import AccountPlusOutline from "mdi-material-ui/AccountPlusOutline";
 import AccountOutline from "mdi-material-ui/AccountOutline";
+import Shopping from "mdi-material-ui/Shopping";
 
 // ** Type import
 import { VerticalNavItemsType } from "src/@core/layouts/types";
+import { useContext } from "react";
+import { UserContext } from "src/@core/context/UserContext";
 
 const navigation = (): VerticalNavItemsType => {
+  const funcionario = useContext(UserContext);
+  let user;
+
+  if (funcionario.user) {
+    user = funcionario.user;
+  }
   return [
-    {
-      title: "Imóveis",
-      icon: HomeOutline,
-      path: "/lista-imoveis",
-    },
-    {
-      title: "Funcionários",
-      icon: AccountOutline,
-      path: "/funcionarios",
-    },
-    {
-      title: "Login",
-      icon: Login,
-      path: "/login",
-      openInNewTab: true,
-    },
-    {
-      title: "Register",
-      icon: AccountPlusOutline,
-      path: "/pages/register",
-      openInNewTab: true,
-    },
+    { title: "Anúncios", icon: Shopping, path: "/" },
+    ...(user?.id
+      ? [
+          {
+            title: "Imóveis",
+            icon: HomeOutline,
+            path: "/lista-imoveis",
+          },
+        ]
+      : []),
+    ...(user?.id
+      ? [
+          {
+            title: "Funcionários",
+            icon: AccountOutline,
+            path: "/funcionarios",
+          },
+        ]
+      : []),
+    ...(!user?.id
+      ? [
+          {
+            title: "Login",
+            icon: Login,
+            path: "/login",
+          },
+        ]
+      : []),
   ];
 };
 
