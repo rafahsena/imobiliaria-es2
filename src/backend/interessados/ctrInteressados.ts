71fe4830362ed628e8c619919d1bd8e11f9fa6bd
Rafael Sena <rafaelsenna13@gmail.com>
2022-10-31 20:15:54 -0300

    feat: mudança nos nomes dos métodos

--- src/backend/interessados/ctrInteressados.ts
+++ src/backend/interessados/ctrInteressados.ts
@@ -3,24 +3,32 @@ import { Interessado, PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export const listarInteressados = async (id: number) => {
-  const interessadosOnAnuncio = await prisma.interessadosOnAnuncios.findMany({ where: { anuncioId: id }, include: { interessado: true } });
+  const interessadosOnAnuncio = await prisma.interessadosOnAnuncios.findMany({
+    where: { anuncioId: id },
+    include: { interessado: true },
+  });
   return interessadosOnAnuncio.map((interessadoOnAnuncio) => {
-    return {...interessadoOnAnuncio.interessado}
+    return { ...interessadoOnAnuncio.interessado };
   });
 };
 
-export const demonstrarInteresse = async (interessado: Interessado, anuncioId: number) => {
-  const {id: interessadoId} = await prisma.interessado.create({ data: {
-    nome: interessado.nome,
-    email: interessado.email,
-    telefone: interessado.telefone
-  } });
-  const interessadoOnAn = await prisma.interessadosOnAnuncios.create({ data: 
-    { anuncioId, interessadoId } 
-  })
-  return interessadoOnAn
-}
+export const demonstrarInteresse = async (
+  interessado: Interessado,
+  anuncioId: number
+) => {
+  const { id: interessadoId } = await prisma.interessado.create({
+    data: {
+      nome: interessado.nome,
+      email: interessado.email,
+      telefone: interessado.telefone,
+    },
+  });
+  const interessadoOnAn = await prisma.interessadosOnAnuncios.create({
+    data: { anuncioId, interessadoId },
+  });
+  return interessadoOnAn;
+};
 
-export const deletarInteressado = async (id: number) => {
-  return await prisma.interessado.delete({ where: { id } })
-}
+export const removerInteressado = async (id: number) => {
+  return await prisma.interessado.delete({ where: { id } });
+};
