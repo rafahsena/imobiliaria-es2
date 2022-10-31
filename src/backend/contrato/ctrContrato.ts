9dee0ced98f342584c8d7707a4823854bf014ecf
Antonio Hamilton <antoniohamilton.s.freitas@gmail.com>
2022-10-31 19:22:15 -0300

    fix: adjustments on emitirContrato and getContrato

--- src/backend/contrato/ctrContrato.ts
+++ src/backend/contrato/ctrContrato.ts
@@ -1,4 +1,4 @@
-import { PrismaClient, Contrato } from "@prisma/client";
+import { PrismaClient, Contrato, Cliente, Endereco } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
@@ -7,13 +7,26 @@ export const listarContratos = async () => {
 }
 
 export const getContrato = async (id: number) => {
-  return await prisma.contrato.findFirst({where: {id}});
+  return await prisma.contrato.findFirst({where: {id}, select: {
+    cliente: true,
+    imovel: { select: { endereco: true, funcionario: true } },
+  }});
 }
 
-export const emitirContrato = async (contrato: Contrato) => {
-  return await prisma.contrato.create({
-    data: contrato,
+export const emitirContrato = async (body: {contrato: Contrato, cliente: Cliente, endereco: Endereco}) => {
+  const endereco = await prisma.endereco.create({
+    data: body.endereco
+  })
+
+  const cliente = await prisma.cliente.create({
+    data: {...body.cliente, enderecoId: endereco.id}
+  })
+
+  const contrato = await prisma.contrato.create({
+    data: { ...body.contrato, clienteId: cliente.id }
   });
+
+  return { endereco, cliente, contrato }
 };
 
 export const revogarContrato = async (id: number) => {
